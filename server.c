1141569978d823082be69e5f14535725e4405691
ge92hol <ge92hol@mytum.de>
2025-01-25 15:22:19 +0100

    added multiple threads server

--- server.c
+++ server.c
@@ -18,19 +18,17 @@ typedef struct entry {
 
 pthread_rwlock_t rwlock = PTHREAD_RWLOCK_INITIALIZER;
 
-
 typedef struct {
     char operation[MAX_KEY_LEN];
     char key[MAX_KEY_LEN];
     char value[MAX_VALUE_LEN];
     int response_ready;
-    sem_t access_semaphore; 
+    sem_t access_semaphore;
 } shm_data_t;
 
 shm_data_t *shm_ptr;
 
 entry_t **hash_table;
-
 
 void initialize_hash_table(int size) {
     hash_table = malloc(size * sizeof(entry_t*));
@@ -38,7 +36,6 @@ void initialize_hash_table(int size) {
         hash_table[i] = NULL;
     }
 }
-
 
 unsigned int hash(const char* key, int size) {
     unsigned int hash = 0;
@@ -48,48 +45,47 @@ unsigned int hash(const char* key, int size) {
     return hash % size;
 }
 
-
 void insert(const char* key, const char* value, int size) {
     unsigned int index = hash(key, size);
-    
-    pthread_rwlock_wrlock(&rwlock); 
-    
+
+    pthread_rwlock_wrlock(&rwlock);
+
     entry_t* new_entry = malloc(sizeof(entry_t));
     strncpy(new_entry->key, key, MAX_KEY_LEN);
     strncpy(new_entry->value, value, MAX_VALUE_LEN);
     new_entry->next = hash_table[index];
     hash_table[index] = new_entry;
-    
-    pthread_rwlock_unlock(&rwlock); 
-}
 
+    pthread_rwlock_unlock(&rwlock);
+}
 
 char* get(const char* key, int size) {
     unsigned int index = hash(key, size);
-    
-    pthread_rwlock_rdlock(&rwlock);  
-    
+
+    pthread_rwlock_rdlock(&rwlock);
+
     entry_t* entry = hash_table[index];
     while (entry) {
         if (strcmp(entry->key, key) == 0) {
-            pthread_rwlock_unlock(&rwlock);  
+            pthread_rwlock_unlock(&rwlock);
             return entry->value;
         }
         entry = entry->next;
     }
-    
-    pthread_rwlock_unlock(&rwlock);  
+
+    pthread_rwlock_unlock(&rwlock);
     return NULL;
 }
 
 
 void delete(const char* key, int size) {
     unsigned int index = hash(key, size);
-    
+
     pthread_rwlock_wrlock(&rwlock); 
-    
+
     entry_t* prev = NULL;
     entry_t* current = hash_table[index];
+
     while (current) {
         if (strcmp(current->key, key) == 0) {
             if (prev) {
@@ -98,21 +94,21 @@ void delete(const char* key, int size) {
                 hash_table[index] = current->next;
             }
             free(current);
-            pthread_rwlock_unlock(&rwlock); 
+            pthread_rwlock_unlock(&rwlock);
             return;
         }
         prev = current;
         current = current->next;
     }
-    
-    pthread_rwlock_unlock(&rwlock); 
+
+    pthread_rwlock_unlock(&rwlock);
 }
 
 
 void* handle_request(void* arg) {
     int size = *(int*)arg;
     while (1) {
-        sem_wait(&shm_ptr->access_semaphore); 
+        sem_wait(&shm_ptr->access_semaphore);
 
         if (strcmp(shm_ptr->operation, "insert") == 0) {
             insert(shm_ptr->key, shm_ptr->value, size);
@@ -129,19 +125,21 @@ void* handle_request(void* arg) {
             snprintf(shm_ptr->value, MAX_VALUE_LEN, "Deleted: %s", shm_ptr->key);
         }
 
-        shm_ptr->response_ready = 0; 
+        shm_ptr->response_ready = 0;
     }
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <hash_table_size>\n", argv[0]);
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <hash_table_size> <number_of_threads>\n", argv[0]);
         return 1;
     }
 
     int hash_table_size = atoi(argv[1]);
-    if (hash_table_size <= 0) {
-        fprintf(stderr, "Error: Invalid hash table size.\n");
+    int num_threads = atoi(argv[2]);
+
+    if (hash_table_size <= 0 || num_threads <= 0) {
+        fprintf(stderr, "Error: Invalid hash table size or number of threads.\n");
         return 1;
     }
 
@@ -149,14 +147,19 @@ int main(int argc, char* argv[]) {
     ftruncate(shm_fd, sizeof(shm_data_t));
     shm_ptr = mmap(0, sizeof(shm_data_t), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
 
-    sem_init(&shm_ptr->access_semaphore, 1, 1); 
+    sem_init(&shm_ptr->access_semaphore, 1, 1);
 
     initialize_hash_table(hash_table_size);
 
-    pthread_t server_thread;
-    pthread_create(&server_thread, NULL, handle_request, &hash_table_size);
+    pthread_t* server_threads = malloc(num_threads * sizeof(pthread_t));
+    for (int i = 0; i < num_threads; i++) {
+        pthread_create(&server_threads[i], NULL, handle_request, &hash_table_size);
+    }
 
-    pthread_join(server_thread, NULL);
+    for (int i = 0; i < num_threads; i++) {
+        pthread_join(server_threads[i], NULL);
+    }
 
+    free(server_threads);
     return 0;
 }
